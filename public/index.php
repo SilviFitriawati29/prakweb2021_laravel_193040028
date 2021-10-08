71d781ba062a6f67e9f2b4bccb68922f0f277952
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-10-08 08:13:02 +0700

    Belajar Laravel Mengenai Blade Templating Engine

--- public/index.php
+++ public/index.php
@@ -16,40 +16,29 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+if (file_exists(__DIR__ . '/../storage/framework/maintenance.php')) {
+    require __DIR__ . '/../storage/framework/maintenance.php';
 }
 
 /*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
+@@ -31,7 +31,7 @@
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+
+require __DIR__ . '/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+@@ -44,7 +44,7 @@
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
     $request = Request::capture()
 ))->send();
-
 $kernel->terminate($request, $response);
---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+@section('container')
+
+<h1 class="m-auto">Halaman About</h1>
+<h3>{{ $name }}</h3>
+<p>{{ $email }}</p>
+<img src="img/{{ $image }}" alt="<?= $name ?>" width="200" class="rounded-circle  d-block" />
+@endsection 
