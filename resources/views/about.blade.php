e3d904f0ebfd1791a5304a9016d6604b3d10869e
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-10-14 23:52:14 +0700

    Membuat Model, Collection & Controller sesuai dengan Video ke 1

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
 <h1 class="m-auto">Halaman About</h1>
 <h3>{{ $name }}</h3>
 <p>{{ $email }}</p>
-<img src="img/{{ $image }}" alt="<?= $name ?>" width="200" class="rounded-circle  d-block" />
+<img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
 @endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,60 +26,10 @@ Route::get('/about', function () {
     return view('about', [
         "title" => "About",
         "name" => "Silvi Fitriawati",
-        "email" => "Silvifitriawati29@gmail.com",
-        "image" => "foto.jpg"
+
     ]);
 });
 
+Route::get('/blog', [PostController::class, 'index']);
 
-
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "author" => "Silvi Fitriawati",
-            "slug" => "judul-post-pertama",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus omnis aspernatur adipisci pariatur magni? Reiciendis quas,,"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "author" => "Kartika Retnowati",
-            "slug" => "judul-post-kedua",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus omnis aspernatur adipisci pariatur magni? Reiciendis quas.."
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// Halaman Single Post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "author" => "Silvi Fitriawati",
-            "slug" => "judul-post-pertama",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus omnis aspernatur adipisci pariatur magni? Reiciendis quas, consectetur vitae tenetur corporis deleniti assumenda rerum numquam quisquam nesciunt provident consequuntur, voluptate autem animi, sapiente eum odio dolore veniam exercitationem magni illo. Ex itaque quaerat blanditiis non eius possimus expedita! Cum officia quia cumque, obcaecati vel quo voluptas odio ipsum error nesciunt illo necessitatibus deleniti voluptatibus maiores assumenda deserunt beatae tenetur ab reiciendis quas ex qui! Tempora sequi officiis doloremque odio incidunt iste?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "author" => "Kartika Retnowati",
-            "slug" => "judul-post-kedua",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus omnis aspernatur adipisci pariatur magni? Reiciendis quas, consectetur vitae tenetur corporis deleniti assumenda rerum numquam quisquam nesciunt provident consequuntur, voluptate autem animi, sapiente eum odio dolore veniam exercitationem magni illo. Ex itaque quaerat blanditiis non eius possimus expedita! Cum officia quia cumque, obcaecati vel quo voluptas odio ipsum error nesciunt illo necessitatibus deleniti voluptatibus maiores assumenda deserunt beatae tenetur ab reiciendis quas ex qui! Tempora sequi officiis doloremque odio incidunt iste? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, ad aspernatur nam quod illum, ducimus facilis eveniet enim omnis beatae asperiores dolor, libero fugit quibusdam deleniti. Amet, aliquam nemo. Necessitatibus earum voluptates minus exercitationem sit cum nulla! Placeat nulla fugit assumenda quidem repudiandae? Modi tenetur itaque consectetur aut voluptatum veritatis harum quia deserunt cupiditate, architecto quaerat iste recusandae veniam! Doloremque aliquam corrupti, enim eos quasi saepe rerum non? Repellendus distinctio modi, dicta beatae nihil illum itaque ex eius deleniti neque, voluptates, voluptatem perspiciatis. Architecto ad distinctio perferendis labore quidem! Atque eligendi voluptatem mollitia autem placeat natus reiciendis, ratione quisquam veniam?"
-        ]
-    ];
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post['slug'] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
