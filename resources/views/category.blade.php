e48fbaa1e8f221ba451453ed6dbed04709c13d4f
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-10-15 15:24:44 +0700

    Membuat Post Category & Eloquent Relationship sesuai dengan Video Ke 4

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.main')
 
 @section('container')
-  
-<h1 class="mb-5">Halaman Blog Posts</h1>
+<h1 class="mb-5">Post Category : {{ $category }}</h1>
   @foreach ($posts as $post)
-    <article class="mb-5">
     <article>
       <h2>
         <a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a>
@@ -12,4 +10,5 @@
       <p>{{ $post->excerpt }}</p>
     </article>
   @endforeach
+
 @endsection
