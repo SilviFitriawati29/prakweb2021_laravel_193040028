778142a9dc0bc7e170bea394fce272991565c35c
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-10-15 12:20:32 +0700

    Membuat Post Model sesuai dengan Video ke 3

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">{{ $post->title }}</h1>
+  
+    <h1 class="mb-5">{{ $post->title }}</h1>
 
-{!! $post->body !!}
+    {!! $post->body !!}
 
-<a href="/blog">Back to Posts</a>
-@endsection 
+  <a href="/blog">Back to Posts</a>
+@endsection  
