f69d68b6d470dc3b9aba07709f1bfc36248be3a5
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-11-19 08:17:39 +0700

    Membuat Dashboard UI Sesuai dengan Video ke 17

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
+use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -39,4 +39,8 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
